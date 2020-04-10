fe4b50cec4cd57fb669e7b93a2b2af7766ab82b7
InesAlejandra <iabn2006@yahoo.com.ar>
2020-04-10 12:41:22 -0500

    ClienteService_Inyección de Dependencia

--- angular/clientes-app/src/app/app.module.ts
+++ angular/clientes-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
 
 @NgModule({
   declarations: [
